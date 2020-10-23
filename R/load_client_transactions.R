0a980d410ca29bdc80bbc1148a377fa4972343db
mannau <mario.annau@quantargo.com>
2020-10-23 17:03:26 +0200

    Parametrize load_client_transaction function

--- R/load_client_transactions.R
+++ R/load_client_transactions.R
@@ -1,9 +1,8 @@
 #' Load client data merged with transactions
 #' @export
-load_client_transactions <- function() {
+load_client_transactions <- function(client, account, trans) {
   M1 = inner_join(client,disp,by="client_id")
   M2 = inner_join(M1,account,by="account_id")
   M3 = inner_join(M2,trans,by="account_id")
-
 }
 
--- inst/berkabank.R
+++ inst/berkabank.R
@@ -2,14 +2,17 @@ library(berkabank)
 
 load("D:/Bernardica/R/trans.rda")
 
-trans_data <- load_client_transactions()
+data("client")
+data("account")
 
-pivot_trans <-create_txn_table(trans_data)
+trans_data <- load_client_transactions(client, account, trans)
+
+pivot_trans <- create_txn_table(trans_data)
 
 str(trans_data)
 
 create_txn_table(trans_data)
 # By Years and months
-top_txn_clients(trans_data)
+# top_txn_clients(trans_data)
 
 
