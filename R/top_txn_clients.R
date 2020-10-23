eb4b8d1da47017284159bfc166d2e584b887409e
Iuliia Guliaeva <VRW5990@uniqa.at>
2020-10-23 17:11:23 +0200

    update of z

--- R/top_txn_clients.R
+++ R/top_txn_clients.R
@@ -7,7 +7,18 @@ top_txn_clients <- function(x) {
     group_by(year, month, client_id) %>%
     summarise(amount_total = sum(amount))
 
-  a <- order(z [ , "amount_total"], decreasing=T)
+  a <- order(z$amount_total, decreasing=T)
   df <- z[a, ]
  head(df, n=10)
   }
+
+# x <-trans_data
+#
+#   z <-x%>%
+#   mutate(date = ymd(date), year = year(date), month = month(date)) %>%
+#   mutate(amount = as.numeric(amount)) %>%
+#   group_by(year, month, client_id) %>%
+#   summarise(amount_total = sum(amount))
+#
+#   as.data.frame(z)
+#   a <- z%>% order(z [ , "amount_total"], decreasing=T)
